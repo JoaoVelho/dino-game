3b828b7e55315fec421786fbd52a68e3452e8925
Joao Velho <joaovictor.rv39@gmail.com>
2021-01-26 16:30:12 -0300

    Game working

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    jump();
+    if (!isJumping) {
+      jump();
+    }
   }
 }
 
 function jump() {
   const initialPos = 350;
-  let position = 0;
+
+  isJumping = true;
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -18,12 +24,13 @@ function jump() {
       let downInterval = setInterval(() => {
         if (position <= 0) {
           clearInterval(downInterval);
+          isJumping = false;
         } else {
           position -= 20;
 
           dino.style.top = initialPos - position + 'px';
         }
-      });
+      }, 20);
     } else {
       //Subindo
       position += 20;
@@ -33,4 +40,31 @@ function jump() {
   }, 20);
 }
 
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = 1000 + 'px';
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      //Game over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
